5c6ebde1f068e947e8cb06650496d735ce52a70d
Ezequiel Rango <erango@proveedor.teco.com.ar>
2023-04-08 13:04:49 -0300

    refactory routes

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,28 @@
 <script setup lang="ts">
-
+import { RouterView } from 'vue-router';
+// import NavBar from '@/shared/components/NavBar.vue';
+// import { routeLinks } from './router/link-routes';
 </script>
+
 
 <template>
   <div class="wrapper">
-    <h1>App</h1>
+    <header>
+      <!-- <NavBar 
+        title="BreakingBad"
+        :links="routeLinks"
+      /> -->
+    </header>
+
+    <main>
+      <RouterView />
+    </main>
+
   </div>
 </template>
 
 <style scoped>
-.wrapper{
+.wrapper {
   display: flex;
   flex-direction: column;
 }
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,26 +1,36 @@
-import AboutPage from '@/shared/pages/AboutPage.vue';
+import { createRouter, createWebHistory } from 'vue-router';
+
 import HomePage from '@/shared/pages/HomePage.vue';
-import {createRouter, createWebHistory} from 'vue-router';
+import AboutPage from '@/shared/pages/AboutPage.vue';
+
+// import { characterRoute } from '@/characters/router';
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
+    history: createWebHistory( import.meta.env.BASE_URL ),
     routes: [
-        //public
-        { path: '/', name : 'home', component: HomePage},
-        { path: '/about', name : 'home', component: AboutPage},
+        // Public
+        { path: '/', name: 'home', component: HomePage },
+        { path: '/about', name: 'about', component: AboutPage },
+        
+        // Characters
+        // {
+        //     ...characterRoute,
+        //     path: '/characters'
+        // },
+        // characterRoutsse,
+        // { 
+        //     path: '/characters', 
+        //     name: 'characters', 
+        //     component: () => import('@/characters/layout/CharacterLayout.vue') 
+        // },
 
-        //characters
-
-        {
-            path: '/characters', 
-            name: 'characters', 
-            component: () => import('@/characters/layout/CharacterLayout.vue')
-        },
-
-        //default
-        {path: '/pathMatch(.*)*', redirect: ()=> ({name: 'home'}) }
-
+        // Default
+        { path: '/:pathMatch(.*)*', redirect: () => ({ name: 'home' }) }
     ]
 });
+
+// path: '/characters',
+// router.addRoute( characterRoute );
+
 
 export default router;
