4417efe31fe7b319e2dbeb840674ba503cbdabd1
Ezequiel Rango <ezequielrango29@gmail.com>
2023-04-08 19:47:28 -0300

    switch api breaking bad for rickAndMorty

--- src/api/breakingBadApi.ts
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const breakingBadApi = axios.create({
-    baseURL: 'https://api.breakingbadquotes.xyz/v1'
-})
-
-export default breakingBadApi;
--- src/characters/components/CardList.vue
+++ src/characters/components/CardList.vue
@@ -1,18 +1,29 @@
 <script setup lang="ts">
-import breakingBadApi from '../../api/breakingBadApi';
+import rickAndMortyApi from '@/api/rickAndMortyApi';
+import type { Character,Result } from '@/characters/interfaces/Character';
+import { ref } from 'vue';
 
-breakingBadApi.get('/quotes')
+const characters = ref<Result[]>([]);
+    
+
+rickAndMortyApi.get<Character>('/character')
     .then( resp => {
-        console.log({data: resp.data[0].author});
+        // characters.value = resp.data;
+        characters.value = resp.data.results
+        console.log(characters.value );
         
     })
 </script>
 
 <template>
     <ul>
-        <li>HOLA</li>
-        <li>HOLA</li>
-        <li>HOLA</li>
+        <li v-for="{id, name} of characters"
+        :key="id"
+        
+        >
+        {{ name }}
+        </li>
+
     </ul>
 </template>
 
