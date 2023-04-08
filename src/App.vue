61dd8946e7f0a4583aa9aa8acb4ae20f881f09fd
Ezequiel Rango <erango@proveedor.teco.com.ar>
2023-04-07 21:14:20 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="wrapper">
+    <h1>hola</h1>
+  </div>
+</template>
+
+<style scoped>
+.wrapper{
+  display: flex;
+  flex-direction: column;
+}
+</style>
