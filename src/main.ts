476f8cc6e51f494d2febaa80bbcbf5c88d01eaef
Ezequiel Rango <ezequielrango29@gmail.com>
2023-04-08 21:53:35 -0300

    install useQuery

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router/index'
-import './assets/main.css'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router/index';
+import { VueQueryPlugin } from '@tanstack/vue-query';
+import './assets/main.css';
 
 createApp(App)
+
+//maneja la cache por nosotros
+.use(VueQueryPlugin)
+
 .use(router)
 .mount('#app')
