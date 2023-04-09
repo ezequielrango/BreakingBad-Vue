ffd7f5b90758ff09ec26d37fd950d30195251675
Ezequiel Rango <ezequielrango29@gmail.com>
2023-04-08 21:36:27 -0300

    composable con estado global

--- src/characters/components/CardList.vue
+++ src/characters/components/CardList.vue
@@ -2,13 +2,14 @@
 import {useCharacters} from '@/characters/composables/useCharacters';
 
 
-const {characters, isLoading} = useCharacters()
+const {characters, isLoading,loadCharacters,hasError,errorMessage} = useCharacters()
 
-
+loadCharacters()
 </script>
 
 <template>
     <h1 v-if="isLoading">Loading...</h1>
+    <p v-if="hasError">{{ errorMessage }}</p>
     <ul>
         <li v-for="character of characters"
         :key="character.id"
--- src/characters/composables/useCharacters.ts
+++ src/characters/composables/useCharacters.ts
@@ -1,25 +1,59 @@
 import rickAndMortyApi from '@/api/rickAndMortyApi';
 import type { Character,Result } from '@/characters/interfaces/Character';
-import { ref } from 'vue';
+import { ref , onMounted} from 'vue';
+import axios from 'axios';
+
+const characters = ref<Result[]>([]);
+const isLoading = ref<boolean>(true);
+const hasError = ref<boolean>(false);
+const errorMessage = ref<string>();
+
 
 
 export const useCharacters = () => {
 
-    // const {data:{results}} = await rickAndMortyApi.get<Character[]>('/character')
-    const characters = ref<Result[]>([]);
-    const isLoading = ref<boolean>(true);
+    onMounted(async() => {
+       await loadCharacters();
+    });
 
-
-    rickAndMortyApi.get<Character>('/character')
-    .then( resp => {
-        // characters.value = resp.data;
-        characters.value = resp.data.results
-        isLoading.value = false;
-        console.log(characters.value );
+    
+    const loadCharacters = async() => {
+        if(characters.value.length > 0) return;
+        isLoading.value = true;
         
-    })
+        try {
+            const {data:{results}} = await rickAndMortyApi.get<Character[]>('/character')
+            characters.value = results;
+            isLoading.value = false;
+
+        } catch (error) {
+            hasError.value = true;
+            isLoading.value = false;
+
+            if(axios.isAxiosError(error)) {
+                return errorMessage.value = error.message;
+            }       
+            errorMessage.value = JSON.stringify(error);
+        }
+    }
+
+
+    // if(characters.value.length === 0){
+    //     rickAndMortyApi.get<Character>('/character')
+    //     .then( resp => {
+    //         // characters.value = resp.data;
+    //         characters.value = resp.data.results
+    //         isLoading.value = false;
+    //         console.log(characters.value );
+            
+    //     })
+    // }
+
     return{
         characters,
         isLoading,
+        loadCharacters,
+        hasError,
+        errorMessage,
     }
 }
